680e111ca5bad442b9609ec62167c3e0fcf58962
mo <thinkgo@aliyun.com>
2019-07-31 13:49:25 +0800

    add shell

---
+++ examples/buildArm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+xgo --targets=linux/arm-5 -ldflags "-s -w" .
+if [ $? -ne 0 ]
+then
+	echo "xgo failed"
+	exit
+fi
+
+#bzip2 -c gmb-linux-arm-5 > anytool.bz2
+#if [ $? -eq 0 ]
+#then
+#    echo "build success"
+#fi
