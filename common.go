9326deeedbc1974d9037b991698fff7882a24b5c
mo <thinkgo@aliyun.com>
2020-03-20 18:50:25 +0800

    update code defined

--- common.go
+++ common.go
@@ -1,3 +1,4 @@
+//go:generate stringer -type=Code -linecomment
 package anytool
 
 import (
@@ -5,64 +6,50 @@ import (
 	"net/http"
 )
 
+type Code int
+
 // code 错误码
 const (
-	CodeSysFeatureNotSupport = 1000 + iota // 不支持
-	CodeSysOperationFailed                 // 操作失败
-	CodeSysInvalidArguments                // 无效参数
-	CodeSysExistYet                        // 已存在
+	CodeFeatureNotSupport Code = 1000 + iota // feature Not support
+	CodeOperationFailed                      // operate failed
+	CodeInvalidArguments                     // Invalid arguments
+	CodeExistYet                             // exist yet
 )
-
-var errMessage = map[int]string{
-	CodeSysFeatureNotSupport: "feature Not support", // 不支持
-	CodeSysOperationFailed:   "Operate failed",
-	CodeSysInvalidArguments:  "Invalid arguments",
-	CodeSysExistYet:          "exist yet",
-}
-
-// codeMessage 根据code返回错误码信息
-func codeMessage(code int) string {
-	var msg string
-
-	if code < 1000 {
-		msg = http.StatusText(code)
-	} else {
-		msg = errMessage[code]
-	}
-
-	if msg == "" {
-		msg = errMessage[CodeSysFeatureNotSupport]
-	}
-
-	return msg
-}
 
 // Response 回复基本格式
 type Response struct {
-	Code    int    `json:"code"`
-	Message string `json:"message,omitempty"`
+	Code    int         `json:"code"`
+	Message string      `json:"message,omitempty"`
+	Data    interface{} `json:"data"`
 }
 
-func response(w http.ResponseWriter, code int) {
-	rsp := Response{Code: code}
-	if code != http.StatusOK {
-		rsp.Message = codeMessage(code)
+func response(w http.ResponseWriter, code Code, data ...interface{}) {
+	var value interface{}
+
+	if len(data) > 0 {
+		value = data[0]
+	} else {
+		value = "{}"
 	}
 
 	if code < 1000 {
-		JSON(w, code, rsp)
+		JSON(w, int(code), Response{
+			Code:    int(code),
+			Message: http.StatusText(int(code)),
+			Data:    value,
+		})
 	} else {
-		JSON(w, http.StatusBadRequest, rsp)
+		JSON(w, int(code), Response{
+			Code:    int(code),
+			Message: code.String(),
+			Data:    value,
+		})
 	}
 }
 
-func responseOK(w http.ResponseWriter, data interface{}) {
-	JSON(w, http.StatusOK, data)
-}
-
 // JSON json传输
-func JSON(w http.ResponseWriter, statusCode int, data interface{}) {
-	content, err := json.Marshal(data)
+func JSON(w http.ResponseWriter, statusCode int, payload interface{}) {
+	content, err := json.Marshal(payload)
 	if err != nil {
 		panic(err)
 	}
